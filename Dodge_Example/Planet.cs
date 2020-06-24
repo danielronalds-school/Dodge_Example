1d36f9426fb32a1bf04446ba430ace992452c84c
danielronalds <danielronalds@mmc.school.nz>
2020-06-25 10:11:32 +1200

    Planets now move

---
+++ Dodge_Example/Planet.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dodge_Example
+{
+    class Planet
+    {
+        // declare fields to use in the class
+        public int x, y, width, height;//variables for the rectangle
+        public Image planetImage;//variable for the planet's image
+
+        public Rectangle planetRec;//variable for a rectangle to place our image in
+        public int score;
+        //Create a constructor (initialises the values of the fields)
+        public Planet(int spacing)
+        {
+            x = spacing;
+            y = 10;
+            width = 20;
+            height = 20;
+            //planetImage contains the plane1.png image
+            planetImage = Properties.Resources.planet1;
+            planetRec = new Rectangle(x, y, width, height);
+        }
+
+        // Methods for the Planet class
+        public void DrawPlanet(Graphics g)
+        {
+            g.DrawImage(planetImage, planetRec);
+            planetRec = new Rectangle(x, y, width, height);
+        }
+
+        public void MovePlanet()
+        {
+            y += 5;
+            planetRec.Location = new Point(x, y);
+        }
+
+    }
+}
