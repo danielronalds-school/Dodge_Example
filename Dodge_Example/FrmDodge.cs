a091a914ca89a371210baf4f4cd93616d11fbb1a
danielronalds <danielronalds@mmc.school.nz>
2020-06-26 11:49:53 +1200

    Added Start Instructions

--- Dodge_Example/FrmDodge.cs
+++ Dodge_Example/FrmDodge.cs
@@ -106,7 +106,8 @@ namespace Dodge_Example
 
         private void FrmDodge_Load(object sender, EventArgs e)
         {
-            lives = int.Parse(txtLives.Text);// pass lives entered from textbox to lives variable
+            MessageBox.Show("Use the left and right arrow keys to move the spaceship. \n Don't get hit by the planets! \n Every planet that gets past scores a point. \n If a planet hits a spaceship a life is lost! \n \n Enter your Name press tab and enter the number of lives \n Click Start to begin", "Game Instructions");
+            txtName.Focus();
         }
 
         private void MnuStart_Click(object sender, EventArgs e)
